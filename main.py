cef88741e8677b81543454bde254936b83b00b14
do-kam <dorothea.kam@gmail.com>
2021-03-28 23:42:04 +0200

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hilllllo, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hilllllooo, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
